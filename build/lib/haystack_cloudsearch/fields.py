36aba4b73d79ac1949358390ba09bac928f57de3
zonedabone <zonedabone@gmail.com>
2013-09-23 00:40:48 -0400

    Github is crazy.

---
+++ build/lib/haystack_cloudsearch/fields.py
@@ -0,0 +1,51 @@
+
+from haystack.fields import CharField, FacetField, IntegerField, MultiValueField
+
+
+class LiteralField(CharField):
+
+    def __init__(self, **kwargs):
+        if kwargs.get('facet_class') is None:
+            kwargs['facet_class'] = FacetLiteralField
+        super(CharField, self).__init__(**kwargs)
+
+
+class FacetLiteralField(FacetField, LiteralField):
+    pass
+
+
+class UnsignedIntegerField(IntegerField):
+
+    def convert(self, value):
+        v = super(UnsignedIntegerField, self).convert(value)
+        if v is None or v < 0:
+            raise TypeError("UnsignedIntegerField does not allow negative integers.")
+        return v
+
+
+class MultiValueCharField(MultiValueField):
+
+    def __init__(self, **kwargs):
+        if kwargs.get('facet_class') is None:
+            kwargs['facet_class'] = FacetMultiValueCharField
+        super(MultiValueField, self).__init__(**kwargs)
+
+
+class FacetMultiValueCharField(FacetField, MultiValueCharField):
+    pass
+
+
+class MultiValueUnsignedIntegerField(MultiValueField):
+    field_type = 'integer'
+
+
+class MultiValueLiteralField(MultiValueCharField):
+
+    def __init__(self, **kwargs):
+        if kwargs.get('facet_class') is None:
+            kwargs['facet_class'] = FacetMultiValueLiteralField
+        super(MultiValueField, self).__init__(**kwargs)
+
+
+class FacetMultiValueLiteralField(FacetField, MultiValueLiteralField):
+    pass
